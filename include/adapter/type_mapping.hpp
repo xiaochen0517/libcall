ed8eaa70690d93cc9e53bf099181636c03114383
mochenya <2827075398@qq.com>
2025-12-01 09:54:54 +0800

    feat: 添加 mapping 头文件

---
+++ include/adapter/type_mapping.hpp
@@ -0,0 +1,13 @@
+#ifndef LIBCALL_TYPE_MAPPING_HPP
+#define LIBCALL_TYPE_MAPPING_HPP
+
+class TypeMapping
+{
+  public:
+    TypeMapping() {};
+    ~TypeMapping() {};
+
+  private:
+};
+
+#endif // LIBCALL_TYPE_MAPPING_HPP
