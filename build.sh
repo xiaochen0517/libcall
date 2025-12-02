34e9c59fc54611d5d61d31c64d9807f47b815667
mochenya <2827075398@qq.com>
2025-12-02 13:44:16 +0800

    feat: update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+BUILD_DIR="build"
+
 cd "$(dirname "$0")"
-cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-cmake --build ./build
+# cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+# cmake --build ./build
+
+if [ ! -f "${BUILD_DIR}/CMakeCache.txt" ]; then
+  cmake -B ${BUILD_DIR} -S . -G Ninja \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+fi
+
+cmake --build ${BUILD_DIR}
